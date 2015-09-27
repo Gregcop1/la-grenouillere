66cf33f0ea92d7269495a5d9407a6949df78c380
Grégory Copin <gregcop1@gmail.com>
2015-09-27 20:02:19 +0200

    fix default configuration

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
-
 <?php
 define('WP_USE_THEMES', true);
-define('WP_CONTENT_DIR', dirname(__FILE__).'/wp-content');
-require(dirname(__FILE__).'/wp/wp-blog-header.php');
-        
+
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
+
