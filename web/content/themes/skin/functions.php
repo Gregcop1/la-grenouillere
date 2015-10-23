5708fa93ee220394817a369635d737b0ae58141d
Grégory Copin <gregcop1@gmail.com>
2015-10-23 23:24:09 +0200

    fix preloader styles

--- web/content/themes/skin/functions.php
+++ web/content/themes/skin/functions.php
@@ -49,7 +49,7 @@ class SkinSite extends TimberSite
     {
         if ($_SERVER['SERVER_NAME'] == 'localhost') {
             echo "<script type='text/javascript' id='__bs_script__'>
-                    document.write(\"<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.9.6.js'><\/script>\".replace(\"HOST\", location.hostname));
+                    document.write(\"<script async src='http://HOST:3000/browser-sync/browser-sync-client.2.9.11.js'><\/script>\".replace(\"HOST\", location.hostname));
                 </script>";
         }
     }
